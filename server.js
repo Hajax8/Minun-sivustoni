017c8e4c30cdad78b9bc69eb7d46e0fc930ff068
Hajax8 <t2luha01@students.oamk.fi>
2023-10-30 15:23:04 +0200

    pääsivu nimetty uudelleen

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")));
 
 app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "MunSivut1.0.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
 });
 
 app.listen(process.env.PORT || 3000, () => console.log("Serveri pyörii..."));
